034cfb164383e0812cb1fe365cdfeecb58a437ab
lhy <3549729258@qq.com>
2026-01-05 15:53:58 +0800

    init

---
+++ backend/src/test/java/com/test/prtest/PrtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.prtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
